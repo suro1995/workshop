32be9c14a5e84916f922d697b8363997f724f999
surong <lxdyran@qq.com>
2016-06-21 16:23:22 +0800

    pass 07cceptsEncodings

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,14 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+    if (this.request.is('json')) {
+        this.response.type = "json";
+        this.response.body = {
+            message: 'hi!'
+        }
+    }
+    else {
+        this.response.body = "ok";
+    }
+    
 })
